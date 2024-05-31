cdb301345bde9ad66f9ad598a3349423e6c3bfc3
Adrian Lim <adrianlimws@gmail.com>
2024-05-31 18:09:03 +1200

    Missing - Use File System Access API to store and access to files on disk. for PWA feature

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 import mkcert from 'vite-plugin-mkcert'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     server: { https: true },
